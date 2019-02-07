5a732e53170043c13d1a7bc462c4ed4c269d02cc
James Condon <c00207200@itcarlow.ie>
2019-02-07 18:04:25 +0000

    state machine up and running

---
+++ src/States/Attack.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "../Components/StateComponent.h"
+
+
+class Attack : public StateComponent
+{
+public:
+
+	Attack() { ; }
+	~Attack() { ; }
+
+};
---
+++ src/States/Idle.cpp
@@ -0,0 +1,16 @@
+#include "Idle.h"
+#include "SearchGun.h"
+#include "Dead.h"
+
+
+
+void Idle::searchGun(Animation * a)
+{
+	a->setCurrent(new SearchGun());
+}
+
+
+void Idle::dead(Animation * a)
+{
+	a->setCurrent(new Dead());
+}
