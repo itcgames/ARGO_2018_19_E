7635613241414703094a7f558a6f3a2ce1fcb991
Peter Daly <peterd060397@gmail.com>
2019-02-20 20:32:52 +0000

    gunDrops

--- src/Menus/playScreen.h
+++ src/Menus/playScreen.h
@@ -41,17 +41,23 @@ public:
 	AudioObserver* m_audioObserver;
 	
 	void checkRoundOver();
+	bool onlineRoundOver();
+	void endRound();
 	void initialiseText(std::string message);
+
+	void deleteGuns();
+	void spawnGuns();
 
 	std::vector<Hand *> m_leftHands;
 	std::vector<Hand *> m_rightHands;
 	std::vector<Player *> m_players;
 	std::vector<AI *> m_aiCharacters;
 	std::vector<Player *> m_networkCharacters;
-	Gun* pistol;
+	/*Gun* pistol;
 	Gun* shotgun;
 	Gun* juicer;
-	Gun* grenade;
+	Gun* grenade;*/
+	std::vector<Gun*> m_guns;
 	std::vector<Entity*> m_ents;
 	Light* testLight;
 
@@ -60,7 +66,7 @@ public:
 	HealthSystem m_hs;
 	ControlSystem m_cs;
 	RenderSystem m_rs;
-	GunSystem m_guns;
+	GunSystem m_gunSys;
 
 
 	PhysicsSystem m_ps;
@@ -99,4 +105,9 @@ public:
 	bool* m_online = false;
 
 	bool m_multiplayer = false;
+
+private: 
+	int m_gunCounter = 0;
+	const int SPAWN_NEW_GUN = 1000;
+
 };
