c2a0d7380d8470b1b465a0930a787858db2c9d57
Peter Daly <peterd060397@gmail.com>
2019-02-21 16:07:29 +0000

    Added credits screen

--- src/Menus/CreditScreen.h
+++ src/Menus/CreditScreen.h
@@ -1,14 +1,34 @@
 #pragma once
 #include <SDL.h>
 #include <SDL_image.h>
+#include "State.h"
+#include <iostream>
+#include <vector>
+#include <SDL_ttf.h>
 
-class CreditScreen
+using namespace std;
+
+class CreditScreen : State
 {
 public:
-	CreditScreen();
-	~CreditScreen();
+	CreditScreen(GameState * state, SDL_Renderer * renderer, TTF_Font* Font, TTF_Font* menuFont, SDL_GameController* controller);
+	~CreditScreen() {};
 
-	void update(double dt);
+	void update();
 	void render(SDL_Renderer * renderer);
+
+private:
+	GameState * m_currentGameState;
+
+	vector<string> creditStrings;
+	SDL_Renderer *m_renderer;
+	vector<SDL_Texture*> textures;
+	vector<SDL_Rect*> renderQuads;
+
+	std::string exit_text = "(B) Exit";
+	SDL_Texture* exittexture;
+	SDL_Rect exitRenderQuad;
+
+	SDL_GameController * gameController = NULL;
 };
 
