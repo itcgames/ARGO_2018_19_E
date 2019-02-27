4a2c787d5add2d8ea55ac3ff610ea4bb98816975
Peter Daly <peterd060397@gmail.com>
2019-02-27 10:35:04 +0000

    Fixed server issue with round ending

--- src/Menus/playScreen.cpp
+++ src/Menus/playScreen.cpp
@@ -407,10 +407,6 @@ void PlayScreen::sendPacket(Entity * ent, Client * client) {
 	for (Player * p : m_networkCharacters) {
 		Entity * ent = (Entity*)p;
 		ControlComponent * cc = (ControlComponent*)ent->getCompByType("CONTROL");
-
-		if (cc->getRoundOver()) {
-			m_roundEnd = true;
-		}
 	}
 }
 
@@ -553,6 +549,7 @@ void PlayScreen::endRound() {
 				tag->setScore(0);
 			}
 			SDL_RenderSetScale(m_renderer, 1.0f, 1.0f);
+			m_timer = 5;
 			m_gameOver = false;
 		}
 	}
